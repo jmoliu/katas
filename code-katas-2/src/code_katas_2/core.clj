b996c3ef942aef6cdfda39c8a6f27a1e34e5d964
jmoliu <juama91@hotmail.com>
2014-09-04 16:23:04 -0300

    Update core.clj

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -50,11 +50,11 @@ retorne una nueva coleccion donde el valor es insertado intercalado cada dos arg
 que cumplan el predicado"
 [predicado valor secuencia]
 (lazy-seq
-         	(if (predicado (first secuencia) (ffirst secuencia))
-         	(cons (cons (first secuencia) valor) (intercalar predicado valor (rest secuencia)))
-          
-           (cons (first secuencia) (intercalar predicado valor (rest secuencia)))
-              )))
+               	(if (predicado (first secuencia) (first (rest secuencia)))
+               	(cons (conj '() valor (first secuencia)) (intercalar predicado valor (rest secuencia)))
+                
+                 (cons (first secuencia) (intercalar predicado valor (rest secuencia)))
+                    )))
 
 (defn tartamudeo
 "Escriba una funcion que retorne una secuencia lazy que comprima el tartamudeo de una secuencia de numeros.
